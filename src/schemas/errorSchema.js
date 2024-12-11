3bac892e347c2bb981e2193b1b445b637d297ec2
bhargav <bhargav.akino@hotmail.com>
2024-12-11 12:24:23 +0530

    Created to display errorlist

---
+++ src/schemas/errorSchema.js
@@ -0,0 +1,8 @@
+export const errorSchema = {
+    "phoneNumber": {
+        "__errors": [
+            "Phone number should match the patter xxx-xxx-xxxx",
+            "Length of phone number should be exact 10"
+        ]
+    }
+}
