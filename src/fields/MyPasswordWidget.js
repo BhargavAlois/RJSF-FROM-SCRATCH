69aa555d94c8f7455ff74faec13a605c5d045047
bhargav <bhargav.akino@hotmail.com>
2024-12-13 15:00:54 +0530

    Hotfix: password field in handle change shown as undefined

--- src/fields/MyPasswordWidget.js
+++ src/fields/MyPasswordWidget.js
@@ -6,7 +6,7 @@ export default function MyPasswordWidget(props) {
     <div>
         <label className='form-label'>{props.schema.title}</label>
         {/* <input type="password" onChange={props.onChange}/> */}
-        <input type="password" onChange={(e) => {props.onChange(props.fieldName, e.target)}}/>
+        <input type="password" onChange={(e) => {props.onChange(props.fieldName, e.target.value)}}/>
         {props.errors && props.errors.map((error, index) => (
               <p key={index} className='text-danger m-0'>{error}</p>
             ))}
