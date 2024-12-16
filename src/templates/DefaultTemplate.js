8ba7430fb63b6d32c98428810a957488a5f56bfc
bhargav <bhargav.akino@hotmail.com>
2024-12-16 13:00:58 +0530

    Made identical to MainTemplate

--- src/templates/DefaultTemplate.js
+++ src/templates/DefaultTemplate.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CustomContentTemplate from './CustomContentTemplate';
+import Button from '@mui/material/Button';
 
 export default function DefaultTemplate(props) {
-    const {schema, uiSchema, onSubmit, onError, onSuccess, onChange, formData, errors} = props;
-  
+    const { schema, uiSchema, fields, onSubmit, onError, onSuccess, onChange, formData, errors } = props;
+
     return (
-        <div className="d-flex flex-column mh-100">
+        <div className="d-flex flex-column" style={{maxWidth : "60%", width: "60%"}}>
             <header className="text-center mb-4">
                 <h3>{schema.title || 'Default title'}</h3>
                 <p>{schema.description || 'This is default description of form'}</p>
@@ -13,25 +14,20 @@ export default function DefaultTemplate(props) {
 
             <form
                 onSubmit={onSubmit}
-                className="w-full d-flex flex-column align-items-center justify-content-center p-4 border border-dark rounded"
+                className="d-flex flex-column bg-light align-items-center align-middle justify-content-center p-5 border border-dark border-opacity-25 rounded-4"
                 style={{ overflow: 'auto' }}
             >
                 <CustomContentTemplate
                     formData={formData}
                     uiSchema={uiSchema}
                     schema={schema}
+                    fields={fields}
                     errors={errors}
                     onChange={onChange}
                     onSuccess={onSuccess}
                     onError={onError}
                 />
-                <button
-                    type="submit"
-                    className="btn btn-primary mt-3 w-25"
-                    style={{ maxWidth: '100px' }}
-                >
-                    Submit
-                </button>
+                <Button variant="contained" className='mt-3' color='primary' type='submit'>Submit</Button>
             </form>
 
             <footer className="mt-4 text-center">
