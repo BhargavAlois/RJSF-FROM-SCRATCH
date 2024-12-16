c19f0c9fc40016904ce550560fd8a2646b18104b
bhargav <bhargav.akino@hotmail.com>
2024-12-16 09:00:01 +0530

    Change in onChange method call

--- src/fields/CustomFileWidget.js
+++ src/fields/CustomFileWidget.js
@@ -4,8 +4,8 @@ export default function CustomFileWidget(props) {
   return (
     <div>
     <label>{props.schema.title}</label>
-    {/* <input type="file" onChange={props.onChange}/> */}
-    <input type="file" onChange={(e) => {props.onChange(props.fieldName, e.target.files[0])}}/>
+    <input type="file" onChange={props.onChange}/>
+    {/* <input type="file" onChange={(e) => {props.onChange(props.fieldName, e.target.files[0])}}/> */}
     {props.errors && props.errors.map((error, index) => (
               <p key={index} className='text-danger m-0'>{error}</p>
             ))}
--- src/fields/MyPasswordWidget.js
+++ src/fields/MyPasswordWidget.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 export default function MyPasswordWidget(props) {
-  console.log("props from mypasswordwidget : ", props);
   return (
     <div>
         <label className='form-label'>{props.schema.title}</label>
-        {/* <input type="password" onChange={props.onChange}/> */}
-        <input type="password" onChange={(e) => {props.onChange(props.fieldName, e.target.value)}}/>
+        <input type="password" onChange={props.onChange}/>
+        {/* <input type="password" onChange={(e) => {props.onChange(props.fieldName, e.target.value)}}/> */}
         {props.errors && props.errors.map((error, index) => (
               <p key={index} className='text-danger m-0'>{error}</p>
             ))}
